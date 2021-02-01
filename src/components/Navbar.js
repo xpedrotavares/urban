f997fff6b6221b89a29c4540d49a667a0d9574c6
Pedro Tavares <xpedrotavaresdev@gmail.com>
2021-01-31 22:26:10 -0300

    Creating navbar and searchbar components

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="logo-div">
+        <p className="logo">URBAN.</p>
+      </div>
+      <div className="navbar-up-line-div">
+        <span className="navbar-up-line"></span>
+      </div>
+      <div className="navbar-items">
+        <a className="navbar-item">FEMININO</a>
+        <a className="navbar-item">MASCULINO</a>
+        <a className="navbar-item">HOME</a>
+        <a className="navbar-item">OFERTAS</a>
+      </div>
+      <div className='search-bar'>
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <div className="wrap">
+        <div className="search">
+           <input type="text" className="searchTerm" placeholder="What are you looking for?" />
+        </div>
+     </div>
+    )
+}
+
+export default SearchBar
